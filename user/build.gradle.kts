16a48e14a7efedc80b574b903c3dae412edc3811
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-04 14:37:17 +0900

    Library version appointed -> latest

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -6,12 +6,12 @@ dependencies {
     //JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     //MySQL driver
-    implementation("mysql:mysql-connector-java:8.0.31")
+    implementation("mysql:mysql-connector-java")
     //Security
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.security:spring-security-config:5.7.3")
+    implementation("org.springframework.security:spring-security-config")
     //Validation
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.4")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     //Jwts
     implementation("io.jsonwebtoken:jjwt:0.9.1")
 
