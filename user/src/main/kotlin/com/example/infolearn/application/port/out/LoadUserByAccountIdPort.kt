d01520fcf8dc73778076ba5c0b038e55eb88964b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-03-13 20:18:43 +0900

    LoadUserPort 세분화

--- user/src/main/kotlin/com/example/infolearn/application/port/out/LoadUserByAccountIdPort.kt
+++ user/src/main/kotlin/com/example/infolearn/application/port/out/LoadUserByAccountIdPort.kt
@@ -2,9 +2,7 @@ package com.example.infolearn.application.port.out
 
 import com.example.infolearn.domain.User
 
-interface LoadUserPort {
-
-    fun findByName(name: String): User?
+interface LoadUserByAccountIdPort {
 
     fun findByAccountId(accountId: String): User?
 }
